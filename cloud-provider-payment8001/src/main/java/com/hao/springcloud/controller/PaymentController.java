0dadb52997d35777f9922c78285b093f684bd409
Zhangzhanhao <Zhangzhanhao@zzh.com>
2022-05-04 15:06:50 +0800

    first commit

---
+++ cloud-provider-payment8001/src/main/java/com/hao/springcloud/controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.hao.springcloud.controller;
+
+import com.hao.springcloud.entities.CommonResult;
+import com.hao.springcloud.entities.Payment;
+import com.hao.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(Payment payment){
+        int i = paymentService.create(payment);
+        log.info("*********插入结果：" + i);
+
+        if(i > 0){
+            return new CommonResult(200,"插入数据库成功",i);
+        }else{
+            return new CommonResult(500,"插入数据库失败",null);
+        }
+    }
+
+    @GetMapping(value = "/payment/getPaymentById/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*********插入结果：" + payment + "hhhh");
+
+        if(payment != null){
+            return new CommonResult(200,"查询成功",payment);
+        }else{
+            return new CommonResult(500,"查询失败",null);
+        }
+    }
+}
