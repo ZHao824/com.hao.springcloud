4bb1cc787779f20ad6b05932e7cdbde06abac98b
Zhangzhanhao <Zhangzhanhao@zzh.com>
2022-05-04 17:57:34 +0800

    pull commit

--- cloud-provider-payment8001/src/main/java/com/hao/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/hao/springcloud/controller/PaymentController.java
@@ -22,6 +22,7 @@ public class PaymentController {
         log.info("*********插入结果：" + i);
         log.info("*********mster test：" + i);
         log.info("*********host-fix test：" + i);
+        log.info("*********push test：" + i);
 
         if(i > 0){
             return new CommonResult(200,"插入数据库成功",i);
